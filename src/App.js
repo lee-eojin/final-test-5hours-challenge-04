74f76164fe40a6fbc7c1b879e0dee0efce5d28d7
lee-eojin <lejin0906@snu.ac.kr>
2026-01-05 21:25:42 +0900

    chore: inital setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Console } from "@woowacourse/mission-utils";
+import InputView from "./InputView.js";
+import OutputView from "./OutputView.js";
+
+class App {
+  async play() {
+    
+  }
+}
+
+export default App;
